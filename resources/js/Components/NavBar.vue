7ac6de7b2336f03d0f04968c491ad1c8c7d50aaa
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-05-25 06:59:36 +0200

    Made the NavDrawer component.

--- resources/js/Components/NavBar.vue
+++ resources/js/Components/NavBar.vue
@@ -1,12 +1,21 @@
 <script setup>
+import { ref } from "vue";
 import { Link } from "@inertiajs/vue3";
+import NavDrawer from "../Components/NavDrawer.vue";
+let appear = ref(false);
+let search = ref("");
 </script>
 <template>
-    <v-app-bar  rounded>
-        <v-app-bar-nav-icon>
-        </v-app-bar-nav-icon>
+    <v-app-bar rounded>
+        <v-app-bar-nav-icon @click="appear = !appear"> </v-app-bar-nav-icon>
         <v-spacer></v-spacer>
+        <v-text-field
+            label="Search for a task"
+            class="mt-5"
+            v-model="search"
+        ></v-text-field>
         <v-app-bar-nav-icon icon="mdi-magnify"></v-app-bar-nav-icon>
         <v-app-bar-nav-icon icon="mdi-logout"></v-app-bar-nav-icon>
     </v-app-bar>
+    <NavDrawer v-model="appear" />
 </template>
---
+++ resources/js/Components/NavDrawer.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+<template>
+    <v-navigation-drawer location="top" temporary>
+        <v-list>
+            <v-list-item>Pending</v-list-item>
+            <v-list-item>Working on it!</v-list-item>
+            <v-list-item>Done!</v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
